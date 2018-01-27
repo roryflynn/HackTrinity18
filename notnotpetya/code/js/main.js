20603946fc8d2529eeeccad7bd1427926e0ff882
Rory Flynn <roryflynn@users.noreply.github.com>
2018-01-27 23:03:48 +0000

    Add notnotpetya recon challenge

---
+++ notnotpetya/code/js/main.js
@@ -0,0 +1,72 @@
+// Create a Stripe client
+var stripe = Stripe('pk_test_6pRNASCoBOKtIshFeQd4XMUh');
+
+// Create an instance of Elements
+var elements = stripe.elements();
+
+// Custom styling can be passed to options when creating an Element.
+// (Note that this demo uses a wider set of styles than the guide below.)
+var style = {
+  base: {
+    color: '#32325d',
+    lineHeight: '18px',
+    fontFamily: '"Helvetica Neue", Helvetica, sans-serif',
+    fontSmoothing: 'antialiased',
+    fontSize: '16px',
+    '::placeholder': {
+      color: '#aab7c4'
+    }
+  },
+  invalid: {
+    color: '#fa755a',
+    iconColor: '#fa755a'
+  }
+};
+
+// Create an instance of the card Element
+var card = elements.create('card', {style: style});
+
+// Add an instance of the card Element into the `card-element` <div>
+card.mount('#card-element');
+
+// Handle real-time validation errors from the card Element.
+card.addEventListener('change', function(event) {
+  var displayError = document.getElementById('card-errors');
+  if (event.error) {
+    displayError.textContent = event.error.message;
+  } else {
+    displayError.textContent = '';
+  }
+});
+
+// Handle form submission
+var form = document.getElementById('payment-form');
+form.addEventListener('submit', function(event) {
+  event.preventDefault();
+
+  stripe.createToken(card).then(function(result) {
+    if (result.error) {
+      // Inform the user if there was an error
+      var errorElement = document.getElementById('card-errors');
+      errorElement.textContent = result.error.message;
+    } else {
+      // Send the token to your server
+      stripeTokenHandler(result.token);
+    }
+  });
+});
+
+function stripeTokenHandler(token) {
+  // Insert the token ID into the form so it gets submitted to the server
+  // var form = document.getElementById('payment-form');
+  // var hiddenInput = document.createElement('input');
+  // hiddenInput.setAttribute('type', 'hidden');
+  // hiddenInput.setAttribute('name', 'stripeToken');
+  // hiddenInput.setAttribute('value', token.id);
+  // form.appendChild(hiddenInput);
+
+  // // Submit the form
+  // //form.submit();
+
+  form.innerHTML =`<h3>Thank u for your payment! You will receive decryption codes soon!</h3>`;
+}
---
+++ notnotpetya/description.md
@@ -0,0 +1,7 @@
+Help! I've been hacked by some ransomware! I just turned on my Windows XP machine this morning and this message appeared on the screen:
+
+<img src="/files/3199e1651f2b2b28be433b83f976cd62/ransomware.jpg" class="img-fluid"/>
+
+Who hacked me? The NSA? CIA? Russia? I must know, please help.
+
+Input the flag as `HackTrinity{<name_of_hacker>}`
