02bff19d9c224ffb254776adb02eaa20b76783bf
Rory Flynn <roryflynn@users.noreply.github.com>
2018-02-01 11:23:03 +0000

    Add badssh challenge

---
+++ badssh/printflag.go
@@ -0,0 +1,8 @@
+package main
+
+import "fmt"
+
+func main() {
+    fmt.Printf("===========================\n!Congratulations!\n\n.You solved the challenge, here's your flag:\n\nHackTrinity{fermat_factorisation_is_beautiful}\n\n\n==========================\n")
+
+}
